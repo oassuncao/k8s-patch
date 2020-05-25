403511ce7901001f3135a34447f455c3e6d7e9bf
Silvio Assunção <silvio.junior@me.com.br>
2020-05-25 09:21:08 -0300

    Adding some tests

---
+++ compare/compare_test.go
@@ -0,0 +1,61 @@
+package compare
+
+import (
+	"github.com/oassuncao/k8s-patch/metadata"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"testing"
+)
+
+func TestDeepEqual(t *testing.T) {
+	data := &v1.Service{Spec: v1.ServiceSpec{ClusterIP: "None"}}
+	metadata.SetMetadata(data)
+
+	type args struct {
+		current runtime.Object
+		obj     runtime.Object
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "default",
+			args: args{
+				current: data,
+				obj:     &v1.Service{Spec: v1.ServiceSpec{ClusterIP: "None"}},
+			},
+			want: true,
+		},
+		{
+			name: "change",
+			args: args{
+				current: data,
+				obj:     &v1.Service{Spec: v1.ServiceSpec{ClusterIP: "Other"}},
+			},
+			want: false,
+		},
+		{
+			name: "changedEqual",
+			args: args{
+				current: &v1.Service{Spec: v1.ServiceSpec{ClusterIP: "Other"}},
+				obj:     &v1.Service{Spec: v1.ServiceSpec{ClusterIP: "Other"}},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := DeepEqual(tt.args.current, tt.args.obj)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("DeepEqual() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("DeepEqual() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ metadata/metadata_test.go
@@ -0,0 +1,88 @@
+package metadata
+
+import (
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"testing"
+)
+
+func TestGetAnnotationName(t *testing.T) {
+	SetAnnotationName("test")
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "default",
+			want: "test",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetAnnotationName(); got != tt.want {
+				t.Errorf("GetAnnotationName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSetMetadata(t *testing.T) {
+	type args struct {
+		obj runtime.Object
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "default",
+			args: args{obj: &v1.Service{}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := SetMetadata(tt.args.obj); (err != nil) != tt.wantErr {
+				t.Errorf("SetMetadata() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			service := tt.args.obj.(*v1.Service)
+			if s := service.Annotations[GetAnnotationName()]; s == "" {
+				t.Errorf("The annotation is not in the object")
+			}
+		})
+	}
+}
+
+func Test_getData(t *testing.T) {
+	service := &v1.Service{}
+	SetMetadata(service)
+
+	type args struct {
+		obj runtime.Object
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantEmpty bool
+	}{
+		{
+			name:      "default",
+			args:      args{obj: service},
+			wantEmpty: false,
+		},
+		{
+			name:      "empty",
+			args:      args{obj: &v1.Service{}},
+			wantEmpty: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _ := getData(tt.args.obj)
+			if len(got) != 0 && tt.wantEmpty {
+				t.Errorf("The result is not empty")
+			}
+		})
+	}
+}
